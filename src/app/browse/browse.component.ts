bfc9470057859a1a08f6c3a6a9ab3beba8b1114e
Sauljr144 <saulsuazojr06@gmail.com>
2024-02-26 19:29:08 -0800

    changed services

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -6,6 +6,7 @@ import { BannerComponent } from '../share/components/banner/banner.component';
 import { MovieService } from '../share/services/movie.service';
 import { MovieCarouselComponent } from '../share/components/movie-carousel/movie-carousel.component';
 import { IVideoContent } from '../share/models/video-content.interface';
+import { forkJoin, map } from 'rxjs';
 
 @Component({
   selector: 'app-browse',
@@ -21,13 +22,38 @@ export class BrowseComponent implements OnInit{
   userImg = JSON.parse(sessionStorage.getItem("loggedInUser")!).picture;
   email = JSON.parse(sessionStorage.getItem("loggedInUser")!).email;
 
-  popularMovies: IVideoContent[] = []
+
+  movies: IVideoContent[] = [];
+  tvShows: IVideoContent[] = [];
+  ratedMovies: IVideoContent[] = [];
+  nowPlayingMovies: IVideoContent[] = [];
+  popularMovies: IVideoContent[] = [];
+  topRated: IVideoContent[] = [];
+  upcomingMovies: IVideoContent[] = [];
+
+  source = [
+    this.movieService.getMovies(),
+    this.movieService.getTvShows(),
+    this.movieService.getRatedMovies(),
+    this.movieService.getNowPlayingMovies(),
+    this.movieService.getPopularMovies(),
+    this.movieService.getTopRated(),
+    this.movieService.getUpcomingMovies()
+  ]
 
   ngOnInit(): void {
-    this.movieService.getMovies().subscribe((data) => {
-      console.log(data);
-      this.popularMovies = data.results;
-    })
+
+    forkJoin(this.source).pipe(map(([movies,tvshows,ratedMovies,nowPlayingMovies,popularMovies,topRated,upcomingMovies]) => {
+      return {movies,tvshows,ratedMovies,nowPlayingMovies,popularMovies,topRated, upcomingMovies}
+    })).subscribe((res:any) => {
+      this.movies = res.movies.results as IVideoContent[];
+      this.tvShows = res.tvshows.results as IVideoContent[];
+      this.ratedMovies = res.ratedMovies.results as IVideoContent[];
+      this.nowPlayingMovies = res.nowPlayingMovies.results as IVideoContent[];
+      this.popularMovies = res.popularMovies.results as IVideoContent[];
+      this.topRated = res.topRated.results as IVideoContent[];
+      this.upcomingMovies = res.upcomingMovies.results as IVideoContent[];3
+    });
   }
 
   signOut(){
